b0e3c8778034356a0ad030b1d551511174773829
DimaS <154427437+SamolinkoD@users.noreply.github.com>
2023-12-20 14:12:06 +0200

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,12 +1,12 @@
 # У даному випадку, весь код написано за Вас) Ваша ж задача прописати аннотації до функцій.
 
 
-# ---=============---
+# виконує сумму 
 def add(num1, num2):
     return num1 + num2
 
 print(add(5, 3))
-# ---=============---
+# додає нові елементи в лист (оновлює лист)
 old_list = ["apple", "pear", "banana"]
 
 def add_element_to_list(lst, element):
@@ -14,12 +14,12 @@ def add_element_to_list(lst, element):
     return lst
 
 print(add_element_to_list(old_list, "grape"))
-# ---=============---
+# множеня двух значень
 def multiple(num1, num2):
     return num1 * num2
 
 print(multiple(5.1, 3))
-# ---=============---
+# створюе лист із рандомними 10 числами від 0 до 100
 def create_list_of_random_numbers(length):
     import random
     lst = []
@@ -28,4 +28,4 @@ def create_list_of_random_numbers(length):
     return lst
 
 print(create_list_of_random_numbers(10))
-# ---=============---
+# кінець коду
